d08df89f268fce6f4ef686cfb599c5748fa3a5b3
ridwanNurfauzi <mrnhp123@gmail.com>
2024-06-20 18:36:54 +0700

    perulangan

--- Program.cs
+++ Program.cs
@@ -1,87 +1,31 @@
-﻿// Percabangan
+﻿// Perulangan
 namespace BelajarDotnetCSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            bool kondisi = true;
-
-            if (kondisi)
+            for (int i = 0; i < 7; i += 2)
             {
-                Console.WriteLine("Kondisi benar.");
+                Console.WriteLine($"Perulangan ke-{i}.");
             }
 
-            int angka = 7;
-
-            if (angka % 2 == 0)
+            int n = 10;
+            while (n > 0)
             {
-                Console.WriteLine("Angka genap.");
-            }
-            else
-            {
-                Console.WriteLine("Angka ganjil.");
+                Console.WriteLine($"Hitung mundur {n}.");
+                n--;
             }
 
-            int nilai = 75;
-            char tingkat;
+            Console.Write("Lakukan perulangan do/while (y/t)?");
+            var txt = Console.ReadLine();
 
-            if (nilai >= 90)
-            {
-                tingkat = 'A';
-                Console.WriteLine($"Nilai {nilai}, Tingkat {tingkat}.");
-            }
-            else if (nilai >= 80)
-            {
-                tingkat = 'B';
-                Console.WriteLine($"Nilai {nilai}, Tingkat {tingkat}.");
-            }
-            else if (nilai >= 70)
-            {
-                tingkat = 'C';
-                Console.WriteLine($"Nilai {nilai}, Tingkat {tingkat}.");
-            }
-            else if (nilai >= 60)
-            {
-                tingkat = 'D';
-                Console.WriteLine($"Nilai {nilai}, Tingkat {tingkat}.");
-            }
-            else if (nilai >= 40)
-            {
-                tingkat = 'E';
-                Console.WriteLine($"Nilai {nilai}, Tingkat {tingkat}.");
-            }
-            else
-            {
-                tingkat = 'F';
-                Console.WriteLine($"Nilai {nilai}, Tingkat {tingkat}.");
-            }
-
-            switch (tingkat)
-            {
-                case 'A':
-                    Console.WriteLine("Luar biasa");
-                    break;
-                case 'B':
-                    Console.WriteLine("Bagus");
-                    break;
-                case 'C':
-                    Console.WriteLine("Lumayan");
-                    break;
-                case 'D':
-                    Console.WriteLine("Kurang");
-                    break;
-                case 'E':
-                    Console.WriteLine("Buruk");
-                    break;
-                case 'F':
-                    Console.WriteLine("Sangat buruk");
-                    break;
-
-                default:
-                    Console.WriteLine("Terjadi kesalahan");
-                    break;
-            }
+            if (txt == "y")
+                do
+                {
+                    Console.Write("Ulangi (y/t)?");
+                    txt = Console.ReadLine();
+                } while (txt == "y");
         }
     }
 }
