1ddb2cb5c4edb8756a2af26f82c5a61d35663a20
ridwanNurfauzi <mrnhp123@gmail.com>
2024-06-20 12:46:53 +0700

    variabel, konstanta, dan tipe data

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Value atau nilai pada variabel dengan const tidak dapat diubah
+            const string namaDepan = "M. Ridwan";
+            string namaBelakang = "Nurfauzi";
+            int umur = 17;
+            double beratBadan = 59.5;
+            char jk = 'l';
+            bool sudahLulus = true;
+
+            // Pengubahan nilai pada variabel
+            umur = 18;
+
+            Console.WriteLine("==============Biodata==============\n");
+
+            // Menampilkan nilai dari variabel
+            Console.WriteLine($"Nama Lengkap : {namaDepan} {namaBelakang}");
+            Console.WriteLine($"Umur : {umur} tahun");
+            Console.WriteLine("Jenis Kelamin : " + jk);
+            Console.WriteLine($"Berat Badan : {beratBadan} kg");
+            Console.WriteLine("Sudah Lulus : " + sudahLulus);
         }
     }
 }
