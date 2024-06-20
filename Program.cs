b47b64d580ffaf219c9711a55a3f9b9feccefcb7
ridwanNurfauzi <mrnhp123@gmail.com>
2024-06-20 20:10:20 +0700

    class

--- Program.cs
+++ Program.cs
@@ -1,45 +1,26 @@
-﻿// method
+﻿class Mobil
+{
+    // Ini adalah property
+    int kecepatan;
+    string warna;
+
+    // Ini adalah method
+    void Maju()
+    {
+        Console.WriteLine("Mobil maju");
+    }
+    void Mundur()
+    {
+        Console.WriteLine("Mobil mundur");
+    }
+}
+
 namespace BelajarDotnetCSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Contoh pemanggilan method
-            ContohMethod();
-
-            KatakanHalo("Kk Ridwn");
-
-            Perkenalan("Kk");
-
-            // Contoh pemangilan method dengan argumen bernama
-            Perkenalan(salam: "Hi",nama: "Ridwan");
-
-            Console.WriteLine(Penjumlahan(1, 2)); // output: 3
-        }
-
-        static void ContohMethod()
-        {
-            Console.WriteLine("Ini adalah contoh method.");
-        }
-
-        // Contoh method dengan parameter
-        static void KatakanHalo(string nama)
-        {
-            Console.WriteLine($"Halo, {nama}.");
-        }
-
-        // Contoh method dengan nilai parameter default
-        static void Perkenalan(string nama, string salam = "Assalaamu'alaikum" /* Parameter salam tidak wajib diisi saat pemanggilan */)
-        {
-            Console.WriteLine($"{salam},");
-            Console.WriteLine($"Perkenalkan, nama saya {nama}.");
-        }
-
-        // Contoh method dengan pengembalian nilai
-        static int Penjumlahan(int a, int b)
-        {
-            return a + b;
         }
     }
 }
